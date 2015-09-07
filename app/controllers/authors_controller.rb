c285e31ed8aefc4c5735e6447a8eb4220415f68d
Juli Emmel <justjiffy@gmail.com>
2015-09-06 17:31:39 -0700

    add and edit links are now working, need to add forms

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -11,8 +11,28 @@ class AuthorsController < ApplicationController
   
 
   def new
+  	@author = Author.new
   end
 
   def edit
+  	@author = Author.find(params[:id])
+  end
+
+  def create 
+  	@author = Author.new(author_params)
+  	if @author.save
+  		redirect_to authors_path
+  	else
+  		render :new
+  	end
+  end
+
+  def update
+  	@author = Author.find(params[:id])
+  	if @author.update_attribuates(author_params)
+  		redirect_to authors_path
+  	else
+  		render :edit
+  	end
   end
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,9 +9,36 @@ class BooksController < ApplicationController
   	@authors = Author.all
   end
 
+  def create 
+  	@book = Book.new(book_params)
+  	if @book.save
+  		redirect_to books_path
+  	else 
+  		render :new
+  	end
+  end
+
   def new
+  	@book = Book.new
   end
 
   def edit
+  	@book = Book.find(params[:id])
   end
+
+  def update
+  	@book = Book.find(params[:id])
+  	if @book.update_attributes(book_params)
+  		redirect_to books_path
+  	else
+  		render :edit
+  	end
+  end
+  
+
+  private
+	def book_params
+		params.require(:book).permit(:title, :copyright, :publisher, :desc, :language )
+	end
+
 end
